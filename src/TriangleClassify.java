f3ad81dbfc90925c60c00eea39e6f6265e2993e3
batuanr <tuanpesi@gmail.com>
2021-09-28 14:31:22 +0700

    pass test

--- src/TriangleClassify.java
+++ src/TriangleClassify.java
@@ -1,5 +1,8 @@
 public class TriangleClassify {
     public static String classifyTriangle(int a, int b, int c) {
+        if((a == b && a != c) || (a == c && a != b) || (b == c && c != a)){
+            return "isosceles triangle";
+        }
         return "equilateral triangle";
     }
 }
